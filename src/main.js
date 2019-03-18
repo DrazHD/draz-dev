58309df2d010eeb00bdc3f15d37309ef5521dfba
DrazHD <drazzhd@gmail.com>
2019-03-18 21:42:32 +0200

    change design

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const discordBox = document.querySelector('#discordBox');
+const desc = document.querySelector('.info__body');
+const descText = desc.textContent;
+const socialBoxes = document.querySelectorAll('.info__link-item');
+
+loadEventListeners();
+
+function loadEventListeners() {
+  discordBox.addEventListener('mouseenter', showDiscordTag)
+  discordBox.addEventListener('mouseleave', hideDiscordTag)
+}
+
+function showDiscordTag() {
+  setTimeout(() => {
+    desc.textContent = 'Draz#1000 on Discord';
+  }, 175);
+}
+
+function hideDiscordTag() {
+  desc.textContent = descText;
+}
+
+window.onload = function () {
+  for (let i = 0; i < socialBoxes.length; i++) {
+    setTimeout(function () {
+      socialBoxes[i].style.opacity = '1';
+    }, i * 200);
+  }
+};
